61496e390852e09d314763311e90079fd1b8ca99
Aliezzat431 <aliezzat02222@gmail.com>
2026-01-22 15:06:34 +0200

    Fix CORS middleware

--- middleware.js
+++ middleware.js
@@ -1,15 +1,27 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const res = NextResponse.next();
-
-  res.headers.set("Access-Control-Allow-Origin", "*");
-  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
+  const origin = req.headers.get("origin");
+  console.log(`got`);
+  
   if (req.method === "OPTIONS") {
-    return new NextResponse(null, { status: 204 });
+    return new NextResponse(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": origin || "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS, PATCH",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With",
+        "Access-Control-Max-Age": "86400",
+        "Access-Control-Allow-Credentials": "true",
+      },
+    });
   }
+
+  const res = NextResponse.next();
+  res.headers.set("Access-Control-Allow-Origin", origin || "*");
+  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+  res.headers.set("Access-Control-Allow-Credentials", "true");
 
   return res;
 }
