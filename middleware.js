23bb92638573ee7012a1c5a876fa5f0ca0383399
Aliezzat431 <aliezzat02222@gmail.com>
2026-01-22 14:11:07 +0200

    Add CORS middleware and update settings structure

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const res = NextResponse.next();
+
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return new NextResponse(null, { status: 204 });
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: "/api/:path*",
+};
